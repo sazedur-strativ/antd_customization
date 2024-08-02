615645f779bc01cc1ef58f7215964cae1372787e
sazedur-strativ <sazedur.rahman@strativ.se>
2024-08-02 17:04:52 +0600

    antd customization

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import PracTable from './PracTable';
+import { TableOne } from './TableOne';
+import TableThree from './TableThree';
+import TableTwo from './TableTwo';
+
+
+const App = () => (
+
+  <>
+  <TableOne/>
+  <TableTwo/>
+  <TableThree/>
+  <PracTable/>
+
+  </>
+
+
+
+);
+export default App;
